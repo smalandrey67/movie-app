eb288c9389dd2f2eb744791e2f4961371e2ca3d4
Evgeniia Cema <ecema2012@gmail.com>
2024-08-24 18:13:44 +0300

    feat: button UI element

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { IoPlayOutline } from "react-icons/io5";
+
+import { Button } from "./shared/ui/Button/Button";
+
 export function App() {
-  return <div>movie app</div>;
+  return (
+    <>
+      <div>movie app</div>
+      <Button variation="playback">
+        <IoPlayOutline size={20} background-color={#e0e1dd}/>
+      </Button>
+      <Button variation="action">Text</Button>
+    </>
+  );
 }
