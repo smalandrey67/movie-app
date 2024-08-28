bc0880337b259da5a101012bbd8d8ea12ad1daa3
Evgeniia Cema <ecema2012@gmail.com>
2024-08-28 16:25:51 +0300

    feat: input UI element

--- src/shared/ui/Input/Input.jsx
+++ src/shared/ui/Input/Input.jsx
@@ -1,12 +1,16 @@
+import { forwardRef } from "react";
+
 import { RxMagnifyingGlass } from "react-icons/rx";
 
 import styles from "./Input.module.scss";
 
-export const Input = ({ value }) => {
+export const Input = forwardRef(({ value, placeholder, ...props }, ref) => {
   return (
     <div className={styles.inputWrapper}>
       <RxMagnifyingGlass className={styles.inputLoupe} />
-      <input className={styles.input} value={value} placeholder="Search movies" />
+      <input className={styles.input} ref={ref} value={value} placeholder={placeholder} {...props} />
     </div>
   );
-};
+});
+
+Input.displayName = "Input";
