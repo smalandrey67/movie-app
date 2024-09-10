3a4bce291f440be6f7544a5b15c8e00eb12458e3
andrew <smalandrey67@ukr.net>
2024-09-10 11:21:55 +0300

    fix: review comments

--- src/modules/popular/popular.jsx
+++ src/modules/popular/popular.jsx
@@ -1,9 +1,12 @@
-import { PopularFilmCard } from "./components/popular-film-card/popular-film-card.jsx";
+import { PopularFilmCard } from "./components/popular-film-card/popular-film-card";
+
+import styles from "./popular.module.scss";
 
 export function Popular() {
   return (
-    <div>
-      Popular
+    <div className={styles.popular}>
+      <PopularFilmCard />
+      <PopularFilmCard />
       <PopularFilmCard />
     </div>
   );
