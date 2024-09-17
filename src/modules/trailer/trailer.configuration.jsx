572ee87b068d5eb1ceb6e59986020fb6c31736ca
andrew <smalandrey67@ukr.net>
2024-09-17 21:05:29 +0300

    feat: implement trailer card

---
+++ src/modules/trailer/trailer.configuration.jsx
@@ -0,0 +1,6 @@
+export const TRAILERS_MOCK = [
+  { title: "abc1", img: "https://www.kinofilms.ua/images/photos/w680/363139.jpg" },
+  { title: "abc2", img: "https://www.kinofilms.ua/images/photos/w680/363139.jpg" },
+  { title: "abc3", img: "https://www.kinofilms.ua/images/photos/w680/363139.jpg" },
+  { title: "abc4", img: "https://www.kinofilms.ua/images/photos/w680/363139.jpg" }
+];
